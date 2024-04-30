4b0e985ee6a698277f55ee4c94b82f4f1e5fb366
Millares <millareslazaro1@gmail.com>
2024-04-30 19:30:03 -0400

    salvar avances

---
+++ Second_program.c
@@ -0,0 +1,13 @@
+/* Additional program */
+#include <stdio.h>
+main()
+{
+    int integer1, integer2, sum;          /*declaration*/
+    printf("Enter the first integer:\n"); /*prompt*/
+    scanf("%d", &integer1);               /* read and integer*/
+    printf("Enter second integer:\n");    /*prompt*/
+    scanf("%d", &integer2);               /* read and integer*/
+    sum = integer1 + integer2;            /* assignment for sum*/
+    printf("Sum is %d\n", sum);           /*print sum*/
+    return 0;
+}
---
+++ first_program.c
@@ -0,0 +1,7 @@
+/* A first program in C */
+main()
+{
+    printf("welcome to C! \n");
+}
+
+/* */
