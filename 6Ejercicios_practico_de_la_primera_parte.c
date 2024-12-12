ee64a51587228a355dfcec0d967f177e790bff9b
Millares95 <lazaro.millabart@alum.uca.es>
2024-12-12 14:27:09 +0100

    Resolucion de ejercicios

---
+++ 6Ejercicios_practico_de_la_primera_parte.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int c, a, thisVarible, q76354, number;
+    printf("Introduzca un numero entero\n: ");
+    scanf(" %d", &a);
+
+    if (a != 7)
+        printf("La variable es distinta de 7\n");
+
+    printf("Este es un programa en C\n");
+    printf("Este es un\n programa en C\n");
+
+    printf("Este\n es\n un\n programa\n en\n C\n");
+    printf("Este\t es\t un\t programa\t en\t C\t");
+}
